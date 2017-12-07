5977de03141480f2e29da4f90c6e3136791e9fea
dagilleland <dagilleland@shaw.ca>
2017-12-07 00:30:38 -0700

    Commentary on the CleanCheckBox control

--- WebApp/CustomerServerControls/CleanCheckBox.cs
+++ WebApp/CustomerServerControls/CleanCheckBox.cs
@@ -7,6 +7,15 @@ using System.Web.UI.WebControls;
 
 namespace WebApp.CustomerServerControls
 {
+    /// <summary>
+    /// The CleanCheckBox control extends the <see cref="CheckBox"/> control by providing properties to apply classes and styles to the Input tag.
+    /// </summary>
+    /// <remarks>
+    /// By default, the CheckBox control will apply a CssClass by first wrapping the
+    /// HTML input tag in a span and then assigning the CssClass to the span rather
+    /// than the input element. This server control provides additional properties to
+    /// apply classes and styles directly to the input element.
+    /// </remarks>
     public class CleanCheckBox : CheckBox
     {
         protected override void OnPreRender(EventArgs e)
